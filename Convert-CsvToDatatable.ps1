9522c9ec3c3db76e58fec5ea074da4831ae7affe
Romain LEROI <39105502+RomainLEROI@users.noreply.github.com>
2020-01-02 15:56:21 +0100

    Update Convert-CsvToDatatable.ps1

--- Convert-CsvToDatatable.ps1
+++ Convert-CsvToDatatable.ps1
@@ -20,13 +20,29 @@ $ExportContent | Out-File -FilePath $CsvPath
 
 $ExportContent = Import-Csv -Path $CsvPath
 
-
 foreach ($NoteProperty in ($ExportContent | Get-Member -MemberType NoteProperty)) {
 
-    [Void] $DataTable.columns.add($NoteProperty.Name, [String])
+    $TestValue = ($ExportContent[1]).($NoteProperty.Name)
+
+    if ($null -ne ($TestValue -as [Int])) {
+
+        [Void] $DataTable.columns.add($NoteProperty.Name, [Int])
+
+    } elseif ($null -ne ($TestValue -as [DateTime])) {
+
+        [Void] $DataTable.columns.add($NoteProperty.Name, [DateTime])
+
+    } elseif ($null -ne ($TestValue -as [Bool])) {
+
+        [Void] $DataTable.columns.add($NoteProperty.Name, [Bool])
+
+    } elseif ($null -ne ($TestValue -as [String])) {
+
+        [Void] $DataTable.columns.add($NoteProperty.Name, [String])
+
+    } 
 
 }
-
 
 $Items = ($ExportContent | Measure-Object).Count
 
