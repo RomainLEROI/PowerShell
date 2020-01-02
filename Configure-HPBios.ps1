3b4a4975000c258eeaa0fafa26996d8fb8d4ac1c
Romain LEROI <39105502+RomainLEROI@users.noreply.github.com>
2020-01-02 11:36:19 +0100

    Update Configure-HPBios.ps1

--- Configure-HPBios.ps1
+++ Configure-HPBios.ps1
@@ -1,146 +1,74 @@
+<#
+$Password = "password"
 
-Function Get-BiosConfig($Setting) {
-
-    Try {
-
-        $Config = (Get-WmiObject -Query ("Select CurrentValue,PossibleValues From HP_BIOSEnumeration Where Name='{0}'" -f $Setting) -Namespace ROOT/HP/InstrumentedBIOS -ErrorAction SilentlyContinue) 
-
-        Return $Config
-
-    } Catch {
-
-        Return $null
-
-    }
-
-}
-
-
-Function Apply-BiosConfig($BiosConfig) {
-
-    Write-CMLog -Log $LogFilePath -Value "[+] Applying bios configuration" -Severity 1
-
-    foreach ($Item in $BiosConfig.GetEnumerator() | Sort Key) {
-
-        $ItemIndex = $Item.Key
-
-        $Setting = (($Item.Value).Split(":")[0]).TrimEnd()
-
-        $DesiredValue = ($Item.Value).Split(":")[1].TrimStart()
-
-        $Config = Get-BiosConfig -Setting $Setting
-
-        if ($null -ne $Config) {
-
-            $CurrentValue = $Config.CurrentValue
-
-            Write-CMLog -Log $LogFilePath -Value "Testing [$Setting], current value is [$CurrentValue], desired value is [$DesiredValue]" -Severity 1
-
-            if ($CurrentValue -ne $DesiredValue) {
-
-                $PossibleValues = @()
-
-                foreach ($Item in $($Config.PossibleValues -replace '[{}]', [String]::Empty)) {
-
-                    $PossibleValues += , $Item.TrimStart()
-
-                }
-
-                if ($PossibleValues.Contains($DesiredValue)) {
-
-                    Write-CMLog -Log $LogFilePath -Value "Desired value [$DesiredValue] is correct" -Severity 1
-
-                    $ResultCode = Set-BiosValue -Setting $Setting -Value $DesiredValue
-
-                    if ($ReturnCode -eq 0) {
-
-                        $BiosConfig.Remove($ItemIndex)
-
-                    }
-
-                } else {
-
-                    Write-CMLog -Log $LogFilePath -Value "No match for desired value [$DesiredValue]" -Severity 2
-      
-                    $BiosConfig.Remove($ItemIndex)
-
-                }
-
-            } else {
-
-                Write-CMLog -Log $LogFilePath -Value "Desired value [$DesiredValue] is already set" -Severity 1
-
-                $BiosConfig.Remove($ItemIndex)
-
-            }
-
-        } else {
-
-            Write-CMLog -Log $LogFilePath -Value "No match for setting [$Setting]" -Severity 2
-
-            $BiosConfig.Remove($ItemIndex)
-
-        }
-
-    }
-
-    if ($BiosConfig.Count -eq 0) {
-
-        Write-CMLog -Log $LogFilePath -Value "Bios configuration was successfully applied" -Severity 1
-
-    } else {
-
-        Write-CMLog -Log $LogFilePath -Value "$($BiosConfig.Count) bios configuration was not applied" -Severity 3
-
-        foreach ($Item in $BiosConfig) {
-
-            $Setting = (($Item.Value).Split(":")[0]).TrimEnd()
-
-            Write-CMLog -Log $LogFilePath -Value "$Setting was not applied" -Severity 3
-
-        }
-
-    }
-
-    Return $BiosConfig
+$BiosConfig = @{
+   
+    1  = "Configure Legacy Support and Secure Boot : Legacy Support Disable and Secure Boot Enable"
+    2  = "Virtualization Technology (VTx) : Disable"
+    3  = "Virtualization Technology for Directed I/O (VTd) : Disable"
+    4  = "LAN/WLAN Switching : Enable"
+    5  = "LAN / WLAN Auto Switching : Enabled"
+    6  = "LAN / WLAN Auto Switching : Enable"
+    7  = "UEFI Boot Options : Enable"
+    8  = "Boot Mode : UEFI Native (Without CSM)"
+    9  = "SecureBoot : Enable"
+    10 = "Secure Boot : Enable"
+    11 = "Setup Password : $Password"
 
 }
 
+Configure-HPBios.ps1 -BiosConfig $BiosConfig
 
-Function Set-BiosValue($Setting, $Value) {
-
-    Try {
-
-        Write-CMLog -Log $LogFilePath -Value "Trying to configure $Setting without password" -Severity 1
-
-        $ReturnCode = $($Bios.SetBIOSSetting($Setting, $Value, "<utf-16/>")).return
-
-        if ($ReturnCode -eq 0) {
-
-            Write-CMLog -Log $LogFilePath -Value "$Setting was successfully configured" -Severity 1
-
-        } else {
-
-            Write-CMLog -Log $LogFilePath -Value "Failed to configure $Setting" -Severity 3
-
-            $ReturnCode = $null
-
-        }
-
-        Return $ReturnCode
-
-    } Catch {
-
-        Write-CMLog -Log $LogFilePath -Value "Something went wrong while setting bios password" -Severity 3
-
-        Return $ErrorList.FailedToSetBiosValue
-
-    }
+$BiosConfig = @{
+   
+    1  = "UEFI Boot Options : Enable"
+    2  = "Always Prompt for HP SpareKey Enrollment : Disable"
+    3  = "HP SpareKey : Disable"
+    4  = "TPM Device : Available"
+    5  = "Embedded Security Device : Device available"
+    6  = "TPM State : Enable"
+    7  = "Clear TPM : No"
+    8  = "TPM Activation Policy : No prompts"
+    9  = "Embedded Security Activation Policy : No prompts"
+    10 = "Reset of TPM from OS : Enable"
+    11 = "Reset of Embedded Security Device through OS : Enable"
+    12 = "OS Management of TPM : Enable"
+    13 = "OS management of Embedded Security Device : Enable"
+    14 = "Activate TPM On Next Boot : Enable"
+    15 = "Activate Embedded Security On Next Boot : Enable"
 
 }
 
+Configure-HPBios.ps1 -BiosConfig $BiosConfig -Password $Password
+#>
 
-Function Write-CMLog($LogFilePath, $Value, $Severity) {
+Param (
+
+    [Parameter(Mandatory = $true)]
+    [HashTable] $BiosConfig,
+
+    [Parameter(Mandatory = $false)]
+    [String] $Password = [String]::Empty,
+    
+    [Parameter(Mandatory = $false)]
+    [String] $LogName = "ConfigureBios.log"
+
+)
+
+Function Write-CMLog {
+
+    Param (
+
+        [Parameter(Mandatory = $true)]
+        [String] $LogFilePath,
+
+        [Parameter(Mandatory = $true)]
+        [String] $Value,
+
+        [Parameter(Mandatory = $true)]
+        [Int] $Severity
+
+    )
 
     $Time = -join @((Get-Date -Format "HH:mm:ss.fff"), "+", (Get-WmiObject -Class Win32_TimeZone | Select-Object -ExpandProperty Bias))
 
@@ -156,77 +84,17 @@ Function Write-CMLog($LogFilePath, $Value, $Severity) {
 }
 
 
-Function Get-HPBios() {
+Try { 
 
-    Try { 
-
-        $Bios = Get-WmiObject -Namespace ROOT/HP/InstrumentedBIOS -Class HP_BIOSSettingInterface -ErrorAction SilentlyContinue
+    $TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction SilentlyContinue
         
-        Return $Bios
-
-    } Catch { 
+} Catch { 
         
-        Return $null
+    $TSEnvironment = $null
             
-    }
-
 }
-
-
-Function Get-TSEnvironment() {
-
-    Try { 
-
-        $TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction SilentlyContinue
-        
-        Return $TSEnvironment
-
-    } Catch { 
-        
-        Return $null
-            
-    }
-
-}
-
-
-$BiosConfig = @{
-   
-    1  = "Configure Legacy Support and Secure Boot : Legacy Support Disable and Secure Boot Enable"
-    2  = "Virtualization Technology (VTx) : Disable"
-    3  = "Virtualization Technology for Directed I/O (VTd) : Disable"
-    4  = "LAN/WLAN Switching : Enable"
-    5  = "LAN / WLAN Auto Switching : Enabled"
-    6  = "LAN / WLAN Auto Switching : Enable"
-    7  = "UEFI Boot Options : Enable"
-    8  = "Always Prompt for HP SpareKey Enrollment : Disable"
-    9  = "HP SpareKey : Disable"
-    10 = "TPM Device : Available"
-    11 = "Embedded Security Device : Device available"
-    12 = "TPM State : Enable"
-    13 = "Clear TPM : No"
-    14 = "TPM Activation Policy : No prompts"
-    15 = "Embedded Security Activation Policy : No prompts"
-    16 = "Reset of TPM from OS : Enable"
-    17 = "Reset of Embedded Security Device through OS : Enable"
-    18 = "OS Management of TPM : Enable"
-    19 = "OS management of Embedded Security Device : Enable"
-    20 = "Activate TPM On Next Boot : Enable"
-    21 = "Activate Embedded Security On Next Boot : Enable"
-    22 = "Boot Mode : UEFI Native (Without CSM)"
-    23 = "SecureBoot : Enable"
-    24 = "Secure Boot : Enable"
-    25 = "Setup Password : Password"
-
-}
-
-$TSEnvironment = Get-TSEnvironment
-
-$LogName = "ConfigureBios.log"
-
-$LogFolder = [System.String]::Empty
-     
-if ($TSEnvironment -ne $Null) {
+  
+if ($null -ne $TSEnvironment) {
 
     $LogFolder = $TSEnvironment.Value("_SMSTSLogPath")
 
@@ -238,7 +106,15 @@ if ($TSEnvironment -ne $Null) {
 
 $LogFilePath = Join-Path -Path $LogFolder -ChildPath $LogName
 
-$Bios = Get-HPBios
+Try { 
+
+    $Bios = Get-WmiObject -Namespace ROOT/HP/InstrumentedBIOS -Class HP_BIOSSettingInterface -ErrorAction SilentlyContinue
+        
+} Catch { 
+        
+    $Bios = $null
+            
+}
 
 if ($null -eq $Bios) {
 
@@ -246,10 +122,124 @@ if ($null -eq $Bios) {
 
 }
 
-$AppliedConfig = Apply-BiosConfig -BiosConfig $BiosConfig
+Write-CMLog -Log $LogFilePath -Value "[+] Applying bios configuration" -Severity 1
 
-if ($AppliedConfig.Count -ne 0) {
+foreach ($Item in $BiosConfig.GetEnumerator() | Sort Key) {
 
+    $ItemIndex = $Item.Key
+
+    $Setting = (($Item.Value).Split(":")[0]).TrimEnd()
+
+    $DesiredValue = ($Item.Value).Split(":")[1].TrimStart()
+
+    Try {
+
+        $Config = (Get-WmiObject -Query ("Select CurrentValue,PossibleValues From HP_BIOSEnumeration Where Name='$Setting'") -Namespace ROOT/HP/InstrumentedBIOS -ErrorAction SilentlyContinue) 
+
+    } Catch {
+
+        $Config = $null
+
+    }
+
+    if ($null -ne $Config) {
+
+        $CurrentValue = $Config.CurrentValue
+
+        Write-CMLog -Log $LogFilePath -Value "Testing [$Setting], current value is [$CurrentValue], desired value is [$DesiredValue]" -Severity 1
+
+        if ($CurrentValue -ne $DesiredValue) {
+
+            $PossibleValues = @()
+
+            foreach ($Item in $($Config.PossibleValues -replace '[{}]', [String]::Empty)) {
+
+                $PossibleValues += , $Item.TrimStart()
+
+            }
+
+            if ($PossibleValues.Contains($DesiredValue)) {
+
+                Write-CMLog -Log $LogFilePath -Value "Desired value [$DesiredValue] is correct" -Severity 1
+
+                Try {
+
+                    Write-CMLog -Log $LogFilePath -Value "Trying to configure $Setting without password" -Severity 1
+
+                    if (![String]::IsNullOrEmpty($Password)) {
+
+                        $ReturnCode = $($Bios.SetBIOSSetting($Setting, $DesiredValue, "<utf-16/>" + $Password)).return
+
+                    } else {
+
+                        $ReturnCode = $($Bios.SetBIOSSetting($Setting, $DesiredValue, "<utf-16/>")).return
+
+                    }
+
+                } Catch {
+
+                    Write-CMLog -Log $LogFilePath -Value "Something went wrong while setting bios password" -Severity 3
+
+                    $ReturnCode = 10
+
+                } Finally {
+
+                    if ($ReturnCode -eq 0) {
+
+                        $BiosConfig.Remove($ItemIndex)
+
+                        Write-CMLog -Log $LogFilePath -Value "$Setting was successfully configured" -Severity 1
+
+                    } else {
+
+                        Write-CMLog -Log $LogFilePath -Value "Failed to configure $Setting" -Severity 3
+
+                    }
+
+                }
+
+            } else {
+
+                Write-CMLog -Log $LogFilePath -Value "No match for desired value [$DesiredValue]" -Severity 2
+      
+                $BiosConfig.Remove($ItemIndex)
+
+            }
+
+        } else {
+
+            Write-CMLog -Log $LogFilePath -Value "Desired value [$DesiredValue] is already set" -Severity 1
+
+            $BiosConfig.Remove($ItemIndex)
+
+        }
+
+    } else {
+
+        Write-CMLog -Log $LogFilePath -Value "No match for setting [$Setting]" -Severity 2
+
+        $BiosConfig.Remove($ItemIndex)
+
+    }
+
+}
+
+if ($BiosConfig.Count -eq 0) {
+
+    Write-CMLog -Log $LogFilePath -Value "Bios configuration was successfully applied" -Severity 1
+
+} else {
+
+    Write-CMLog -Log $LogFilePath -Value "$($BiosConfig.Count) bios configuration was not applied" -Severity 3
+
+    foreach ($Item in $BiosConfig) {
+
+        $Setting = (($Item.Value).Split(":")[0]).TrimEnd()
+
+        Write-CMLog -Log $LogFilePath -Value "$Setting was not applied" -Severity 3
+
+    }
+    
     [Environment]::Exit(20)
 
 }
